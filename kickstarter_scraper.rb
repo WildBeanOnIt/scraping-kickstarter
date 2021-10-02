902c9f939e4653df84ac2419d095fa4feddef135
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-10-02 14:55:55 -0500

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,15 @@
 # require libraries/modules here
+require './kickstarter_scraper.rb'
+require 'nokogiri'
+require 'pry'
+
 
 def create_project_hash
+  html = file.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry  
   # write your code here
 end
+
+
+# create_project_hash
